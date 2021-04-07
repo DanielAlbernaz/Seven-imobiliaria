7718c386c998f6108e0a7d40a28e43ffb2c28a6b
Ruthtlidia <ruthtlidia@gmail.com>
2021-04-07 17:35:19 -0300

    Inicio da Pagina Home

--- resources/views/site/main/header.blade.php
+++ resources/views/site/main/header.blade.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
     <title>Seven | Imóbiliaria</title>
 </head>
 <body>
+    <header>
+        <nav class="navtopo">
+            <div class="rectangle">
 
+            </div>
+        </nav>
+    </header>
