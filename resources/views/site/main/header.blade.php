52fd8d3b014b64e8e997e0dfc294daffc1dde1e7
Ruthtlidia <ruthtlidia@gmail.com>
2021-04-08 16:44:43 -0300

    Nav

--- resources/views/site/main/header.blade.php
+++ resources/views/site/main/header.blade.php
@@ -4,14 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles.css">
+    <link href="{{asset('assests/site/css/style.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <title>Seven | Imóbiliaria</title>
 </head>
 <body>
     <header>
         <nav class="navtopo">
+
             <div class="rectangle">
 
+                <div class="telphone">
+                    <div class="icontel">
+                        <img src="./images/tel.png" alt="">
+                    </div>
+                    <div class="number">
+                        <a href=""><font color="white">(XX) XXXX-XXXX</font></a>
+                    </div>
+                </div>
+
+                <div class="locali">
+                    <div class="amet">
+                        <a href=""><font color="white">Lorem ipsum dolor sit amet, consectetur.</font></a>
+                    </div>
+                    <div class="iconamet">
+                        <a href=""><img src="./images/placeholder.png" alt=""></a>
+                    </div>
+                </div>
+
+                <div class="iconredes">
+                    <dd>
+                        <a href=""><i class="fa fa-facebook"></i></a>
+                    </dd>
+                    <dd>
+                        <a href=""><i class="fa fa-instagram"></i></a>
+                    </dd>
+                </div>
+
             </div>
+
         </nav>
     </header>
