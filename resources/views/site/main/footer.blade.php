2fc335757e400e8da6d352f9dd83ffac0747f9d8
DanielAlbernaz <54473406+DanielAlbernaz@users.noreply.github.com>
2021-04-26 21:10:53 -0300

    Ajuste principal

--- resources/views/site/main/footer.blade.php
+++ resources/views/site/main/footer.blade.php
@@ -1,12 +1,17 @@
-
+<?php
+    $empresa = exibirInfoEmpresa();
+    $telefones = exibirTelefone();
+?>
 
 <!-- RODAPÉ -->
 <footer >
     <div class="footer">
         <article class="colun-1 reta">
-            <label for="">
-                <img src="./images/marca.png" alt="">
-            </label>
+            <a href="/" style="cursor: pointer">
+                <label style="cursor: pointer" for="">
+                    <img style="cursor: pointer" src="./images/marca.png" alt="">
+                </label>
+            </a>
             <ul>
                 <li class="a">
                     <a href="">Política de Privacidade</a>
@@ -16,49 +21,54 @@
                 </li>
                 <li class="a">
                     <img src="./images/room.png" alt="">
-                    <a href="">Lorem ipsum dolor sit amet consectetur</a>
+                    <a style="cursor:pointer"> {{ $empresa->endereco }}</a>
                 </li>
+                @for ($i = 0; $i < count($telefones); $i++)
+                    <li class="a">
+                        <i style="color:white" class="fa fa-phone"></i>
+                        <a style="color:white" href="tel:55{{ formatPhone($telefones[$i]['telefone']) }}">{{ $telefones[$i]['telefone'] }} </a>
+                    </li>
+                @endfor
+
                 <li class="a">
-                    <img src="./images/tel.png" alt="">
-                    <a href="">(XX)XXXX-XXXX</a>
-                </li>
-                <li class="a">
-                    <img src="./images/email.png" alt="">
-                    <a href="">conceito@gmail.com</a>
+                    <i class="fa fa-envelope" style="color: white"></i>
+                    <a href="">{{ $empresa->email }}</a>
                 </li>
             </ul>
         </article>
 
+
+
         <article class="colun-2 reta left-colun ">
-            <label for="" class="labeltop"> Serviços</label>
+            {{-- <label for="" class="labeltop"> Serviços</label> --}}
                 <ul class="articul">
                     <li class="a">
-                        <a href="">Lorem ipsum dolor</a>
+                        <a href="/">HOME</a>
                     </li>
                     <li class="a">
-                        <a href="">Lorem ipsum dolor</a>
+                        <a href="institucional">CONTATO</a>
                     </li>
                     <li class="a">
-                        <a href="">Lorem ipsum dolor</a>
+                        <a href="imoveis">IMÓVEIS</a>
                     </li>
                     <li class="a">
-                        <a href="">Lorem ipsum dolor</a>
+                        <a href="institucional" class="">QUEM SOMOS</a>
                     </li>
                 </ul>
         </article>
 
         <article class="colun-4 left-colu">
             <dd>
-                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href="{{ $empresa->facebook }}" target="_blank"><i class="fa fa-facebook"></i></a>
             </dd>
             <dd>
-                <a href=""><i class="fa fa-instagram"></i></a>
+                <a href="{{ $empresa->instagram }}" target="_blank"><i class="fa fa-instagram"></i></a>
             </dd>
         </article>
     </div>
 
     <div class="pe">
-        <label for=""> Todos direitos reservados 2021 </label>
+        <label for=""> COPYRIGHT © 2021. TODOS OS DIREITOS RESERVADOS - SEVEN CORRETORES ASSOCIADOS. </label>
     </div>
 
 </footer>
@@ -69,6 +79,13 @@
  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVt0KZlGvIJYBxTod0Fm33ZL9DriWeSqk&amp;signed_in=true&amp;libraries=places&amp;callback=initMap"></script>
  <script src="{{asset('assests/site/js/custom.js')}}"></script>
  <script src="{{asset('assests/site/js/maps.js')}}"></script>
+ <script src="{{asset('assests/site/js/main.js')}}"></script>
+
+   {{-- Banner js --}}
+   <script type="text/javascript" src="{{asset('assests/site/js/minimalist-slider/assets/js/jquery.minimal-plugins.min.js')}}"></script>
+   <script type="text/javascript" src="{{asset('assests/site/js/minimalist-slider/assets/js/jquery.minimalist-banner.min.js')}}"></script>
+   <script type="text/javascript" src="{{asset('assests/site/js/slider/js/jquery.nivo.slider.js')}}"></script>
+   <script type="text/javascript" src="{{asset('assests/site/js/slider/home.js')}}"></script>
 
  <script>
 
--- resources/views/site/main/header.blade.php
+++ resources/views/site/main/header.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+    <?php
+    $empresa = exibirInfoEmpresa();
+    $telefones = exibirTelefone();
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +12,13 @@
     <link href="{{asset('assests/site/css/style2.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('assests/site/css/main.css')}}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    {{-- css banner --}}
+    <link rel="stylesheet" href="{{asset('assests/site/js/font-awesome/css/font-awesome.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assests/site/js/slider/css/nivo-slider.css')}}" />
+    <link rel="stylesheet" href="{{asset('assests/site/js/slider/css/preview.css')}}" />
+    <link rel="stylesheet" href="{{asset('assests/site/js/slider/css/style.css')}}" />
+
     <title>Seven | Imóbiliaria</title>
 </head>
 <body>
@@ -22,13 +33,14 @@
                         <ul class="icontel">
                             <li class="telwidth">
                                 <a href="">
-                                    <p><i class="fa fa-phone"></i> (XX) XXXX-XXXX</p>
+                                    <p><i style="color:white" class="fa fa-phone"></i> <a style="color:white" href="tel:55{{ formatPhone($telefones[0]['telefone']) }}">{{ $telefones[0]['telefone'] }} </a></p>
+
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a style="cursor:pointer">
                                     <font color="white">
-                                        <i class="fa fa-map-marker"></i> Lorem ipsum dolor sit amet, consectetur.
+                                        <i class="fa fa-map-marker"></i> {{ $empresa->endereco }}
                                     </font>
                                 </a>
                             </li>
@@ -37,10 +49,10 @@
 
                     <div class="iconredes">
                         <dd>
-                            <a href=""><i class="fa fa-facebook"></i></a>
+                            <a href="{{ $empresa->facebook }}" target="_blank"><i class="fa fa-facebook"></i></a>
                         </dd>
                         <dd>
-                            <a href=""><i class="fa fa-instagram"></i></a>
+                            <a href="{{ $empresa->instagram }}" target="_blank"><i class="fa fa-instagram"></i></a>
                         </dd>
                     </div>
 
@@ -51,19 +63,15 @@
         </div>
 
         <nav>
-            
-            <div class="topnav" id="myTopnav">
-                <label for="" class="logo">
-                    <img src="./images/LOGO.PNG" alt="">
-                </label>
+
+            <div class="topnav" id="myTopnav" onclick="redirect()" style="cursor :pointer;" >
+                    <label style="cursor :pointer;"  for="" class="logo">
+                            <img style="cursor :pointer;" src="./images/LOGO.PNG" alt="">
+                    </label>
                 <ul>
-                    <a href=""><img src="./images/turned_in_not.png" alt=""></a>
-                    <a href="#home" class="">Loren</a>
-                    <a href="#news">Loren</a>
-                    <a href="#contact">Loren</a>
-                    <a href="#about">Loren</a>
-                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i>
+                    <a href="institucional">CONTATO</a>
+                    <a href="imoveis">IMÓVEIS</a>
+                    <a href="institucional" class="">QUEM SOMOS</a>
                     </a>
                 </ul>
               </div>
